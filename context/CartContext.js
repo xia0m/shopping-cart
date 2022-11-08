9271376facf255946c9bd71b4e6410479da76b57
xia0m <alexma325@gmail.com>
2022-11-08 15:57:46 -0800

    added infinity scroll

--- context/CartContext.js
+++ context/CartContext.js
@@ -10,52 +10,8 @@ export const CartContext = createContext({
 });
 
 export const CartProvider = ({ children }) => {
-  const mockData = [
-    {
-      id: 1,
-      title: 'Starry Night',
-      description:
-        'High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Goh.',
-      imageUrl: 'https://demo.snipcart.com/images/starry-night.jpg',
-      prices: {
-        physical: '79.95',
-        digital: '29.75',
-      },
-    },
-    {
-      id: 2,
-      title: 'Irises',
-      description: 'Irises is yet again, another painting by the Dutch artist Vincent van Gogh.',
-      imageUrl: 'https://demo.snipcart.com/images/irises.jpg',
-      prices: {
-        physical: '65.95',
-        digital: '37.75',
-      },
-    },
-    {
-      id: 3,
-      title: 'Branches with Almond Blossom',
-      description: 'Branches with Almond Blossom is another van Gogh painted in 1890.',
-      imageUrl: 'https://demo.snipcart.com/images/almond.jpg',
-      prices: {
-        physical: '29.75',
-        digital: '17.75',
-      },
-    },
-    {
-      id: 4,
-      title: 'Rosy-Fingered Dawn at Louse Point',
-      description:
-        "The title Rosy-Fingered Dawn at Louse Point refers to one of Willem de Kooning's favourite places in Long Island.",
-      imageUrl: 'https://demo.snipcart.com/images/rosy.jpg',
-      prices: {
-        physical: '49.95',
-        digital: '24.75',
-      },
-    },
-  ];
   const [cart, setCart] = useState([]);
-  const [productList, setProductList] = useState(mockData);
+  const [productList, setProductList] = useState([]);
   const [promoApplied, setPromoApplied] = useState(false);
 
   return (
--- screens/ProductListScreen.js
+++ screens/ProductListScreen.js
@@ -1,11 +1,92 @@
-import { FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList } from 'react-native';
 import { Div } from 'react-native-magnus';
 
 import ProductCard from '../components/ProductCard';
 import { useCart } from '../context/CartContext';
 
+const mockData = [
+  {
+    id: 1,
+    title: 'Starry Night',
+    description:
+      'High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Goh.',
+    imageUrl: 'https://demo.snipcart.com/images/starry-night.jpg',
+    prices: {
+      physical: '79.95',
+      digital: '29.75',
+    },
+  },
+  {
+    id: 2,
+    title: 'Irises',
+    description: 'Irises is yet again, another painting by the Dutch artist Vincent van Gogh.',
+    imageUrl: 'https://demo.snipcart.com/images/irises.jpg',
+    prices: {
+      physical: '65.95',
+      digital: '37.75',
+    },
+  },
+  {
+    id: 3,
+    title: 'Branches with Almond Blossom',
+    description: 'Branches with Almond Blossom is another van Gogh painted in 1890.',
+    imageUrl: 'https://demo.snipcart.com/images/almond.jpg',
+    prices: {
+      physical: '29.75',
+      digital: '17.75',
+    },
+  },
+  {
+    id: 4,
+    title: 'Rosy-Fingered Dawn at Louse Point',
+    description:
+      "The title Rosy-Fingered Dawn at Louse Point refers to one of Willem de Kooning's favourite places in Long Island.",
+    imageUrl: 'https://demo.snipcart.com/images/rosy.jpg',
+    prices: {
+      physical: '49.95',
+      digital: '24.75',
+    },
+  },
+];
+
 export default function ProductListScreen() {
-  const { productList } = useCart();
+  const { productList, setProductList } = useCart();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchProducts = async () => {
+    setIsLoading(true);
+    // 把IP地址修改成本地计算机的地址
+    const LIMIT = 8;
+    const URL = `http://192.168.50.185:3000/carts?page=${currentPage}&limit=${LIMIT}`;
+    try {
+      const result = await fetch(URL);
+      const data = await result.json();
+      setProductList([...productList, ...data]);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      // 特殊情况，不是最佳写法
+      setProductList(mockData);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, [currentPage]);
+
+  const renderLoader = () => {
+    return isLoading ? (
+      <Div>
+        <ActivityIndicator sie="large" color="#aaa" />
+      </Div>
+    ) : null;
+  };
+
+  const loadMoreItem = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
   return (
     <Div style={{ backgroundColor: 'white', height: '100%' }}>
@@ -13,6 +94,9 @@ export default function ProductListScreen() {
         data={productList}
         renderItem={(item) => <ProductCard data={item} />}
         keyExtractor={(item) => item.id}
+        ListFooterComponent={renderLoader}
+        onEndReached={loadMoreItem}
+        onEndReachedThreshold={0.3}
       />
     </Div>
   );
